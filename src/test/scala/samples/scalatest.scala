5e63f2a361fa41bd9da7c7b78b4cb1516af5aa12
zhangzhikuan <zhangzhikuan@yongche.com>
2017-05-15 01:54:07 +0800

    修改最新的版本

--- src/test/scala/samples/scalatest.scala
+++ src/test/scala/samples/scalatest.scala
@@ -47,33 +47,6 @@ class StackSuite extends Assertions {
   }
 }
 
-/*
-Here's an example of a FunSuite with ShouldMatchers mixed in:
-*/
-import org.scalatest.FunSuite
-import org.scalatest.matchers.ShouldMatchers
-
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-@RunWith(classOf[JUnitRunner])
-class ListSuite extends FunSuite with ShouldMatchers {
-
-  test("An empty list should be empty") {
-    List() should be ('empty)
-    Nil should be ('empty)
-  }
-
-  test("A non-empty list should not be empty") {
-    List(1, 2, 3) should not be ('empty)
-    List("fee", "fie", "foe", "fum") should not be ('empty)
-  }
-
-  test("A list's length should equal the number of elements it contains") {
-    List() should have length (0)
-    List(1, 2) should have length (2)
-    List("fee", "fie", "foe", "fum") should have length (4)
-  }
-}
 
 /*
 ScalaTest also supports the behavior-driven development style, in which you
